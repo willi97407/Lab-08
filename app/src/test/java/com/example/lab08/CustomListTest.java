939d796205a7064f050d4dff070f0f2aaaa646f5
willi97407 <wli14@ualberta.ca>
2026-03-05 17:57:50 -0700

    update CustomListTest

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -15,4 +15,12 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City calgary =  new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+    }
 }
